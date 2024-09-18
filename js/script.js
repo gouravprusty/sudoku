847a2f1efc82baf78981b609a8f38c9d731e12a3
Gourav Prusty <gouravprusty1432@gmail.com>
2024-09-18 12:05:04 +0530

    displaying random numbers in the grid

--- js/script.js
+++ js/script.js
@@ -59,11 +59,65 @@ const getPlayerName = () => localStorage.getItem("player_name")
 
 const showTime = (seconds) => new Date(seconds * 1000).toISOString().substr(11, 8);
 
+const clearSudoku = () => {
+    for (let i = 0; i < Math.pow(CONSTANT.GRID_SIZE, 2); i++) {
+        cells[i].innerHTML = "";
+        cells[i].classList.remove("filled");
+        cells[i].classList.remove("selected");
+    }
+}
+
 const initSudoku = () => {
+    clearSudoku();
     su = generateSudoku(level);
     su_ans = [...su.question];
     console.table(su_ans);
+    for (let i = 0; i < Math.pow(CONSTANT.GRID_SIZE, 2); i++) {
+        let row = Math.floor(i/CONSTANT.GRID_SIZE);
+        let col = i%CONSTANT.GRID_SIZE;
+        cells[i].setAttribute("data-value", su.question[row][col]);
+        if(su.question[row][col] !== 0){
+            cells[i].classList.add("filled");
+            cells[i].innerHTML = su.question[row][col]
+        }
+    }
 };
+
+const hoverBg = (index) => {
+    let row = Math.floor(index / CONSTANT.GRID_SIZE);
+    let col = index % CONSTANT.GRID_SIZE;
+    let box_start_row = row - row % 3;
+    let box_start_col = col - col % 3;
+    for (let i = 0; i < CONSTANT.BOX_SIZE; i++) {
+        for (let j = 0; j < CONSTANT.BOX_SIZE; j++) {
+            let cell = cells[9 * (box_start_row + i) + (box_start_col + j)];
+            cells.classList.add("hover");
+        }
+    }
+    let step = 9;
+    while(index - step >= 0){
+        cells[index - step].classList.add("hover");
+        step += 9;
+    }
+    step = 9;
+    while(index + step < 81){
+        cells[index + step].classList.add("hover");
+        step += 9;
+    }
+    step = 1;
+    while(index - step >= 9*row){
+        cells[index - step].classList.add("hover");
+        step += 1;
+    }
+    step = 1;
+    while(index + step < 9*row  + 9){
+        cells[index + step].classList.add("hover");
+        step += 9;
+    }
+}
+const resetBg = () => {
+    cells.forEach(e => e.classList.remove("hover"));
+}
 
 const startGame = () => {
     startScreen.classList.remove("active");
